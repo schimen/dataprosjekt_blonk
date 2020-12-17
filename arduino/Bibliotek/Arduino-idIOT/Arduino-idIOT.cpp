f60dd37432813601c9cf9b3378483d9473b5be7b
NamesSid <Rkkeys@hotmail.com>
2020-12-17 19:43:40 +0100

    Kommentarer

--- arduino/Bibliotek/Arduino-idIOT/Arduino-idIOT.cpp
+++ arduino/Bibliotek/Arduino-idIOT/Arduino-idIOT.cpp
@@ -8,20 +8,20 @@ idIOT::idIOT(String server) {
 }
 
 bool idIOT::connectWiFi(const char* ssid, const char* password, int pause) {
-  WiFi.begin(ssid, password);
-  for (int i = 0; i < 15 && WiFi.status() != WL_CONNECTED; i++) {
-    delay(pause);
+  WiFi.begin(ssid, password);  //nettverket og passordet 
+  for (int i = 0; i < 15 && WiFi.status() != WL_CONNECTED; i++) { //Søker kontakt med nettverket
+    delay(pause);                                                 //med 1 sekund pause  
   }
-  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED) { //Når det er kontakt med nettverket true
     return true;
   }
-  else {
+  else { //Er ikke kontakt med nettverket false
     return false;
   }
 }
 
 void idIOT::onMessageCallback(WebsocketsMessage message) {
-  String msg = message.data();
+  String msg = message.data(); //byte blir til String
   messageHandler(msg);
 }
 
@@ -53,15 +53,15 @@ void idIOT::send(String sendData) {
   client.send(sendData);
 }
 
-void idIOT::sendSerial() {
-  String serialData;
+void idIOT::sendSerial() {// Her skriver man ord som blir sendt fra klienten
+  String serialData;     //skriver på serie monitoren. 
   serialData = Serial.readString();
   client.send(serialData);
 }
 
-void idIOT::update() {
-  client.poll();
-  if (!client.available()) {
+void idIOT::update() {    //I denne koden vil se om det er mer data som skal leses.
+  client.poll();          
+  if (!client.available()) {  //Hvis det ikke er mer data
     Serial.println("Disconnected from server retrying.");
     client.connect(websocketServer_);
   }
